d35c2ab3763bf687e2f65c442f93b2e5f67e6322
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-01-13 08:59:07 +0700

    demo exercise

---
+++ Bai01d/Bai01d.LessionBMI/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Bai01d.LessionBMI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        double weight = 0;
+        double height = 0;
+        
+        Console.Write("Input weight(kg): ");
+        weight = double.Parse(Console.ReadLine());
+        
+        Console.Write("Input height(cm): ");
+        height = double.Parse(Console.ReadLine());
+        
+        double bmi = weight / (height * height);
+        
+        Console.WriteLine("BMI = {0}", bmi);
+        
+        Console.WriteLine("Input random key to exit");
+        Console.ReadKey();
+    }
+}
